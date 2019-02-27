954802249f3d61f361905922ce227535c3bf3e04
megansimmons <simmons.megan584@gmail.com>
2019-02-27 12:17:13 -0600

    class selectors exercise complete

--- js/my-custom-javascript.js
+++ js/my-custom-javascript.js
@@ -1,5 +1,18 @@
 "use strict";
-
 $(document).ready(function(){
     alert('The DOM has finished loading!');
+//put all code in here...it is more important than an iife
+
+
+//Exercise ID Selectors
+// var contents = $('#lister').html();
+// alert(contents);
+
+
+//Exercise Class Selectors
+$('.codeup').css('border', '1px solid red');
+
+
+
+
 });
